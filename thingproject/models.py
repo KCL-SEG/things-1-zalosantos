ed5bd124391172d0da57a2290c6e3be57c5074f1
Gonçalo  Azevedo <zalosantos02@gmail.com>
2022-10-04 09:02:14 +0100

    silly mistake

--- thingproject/models.py
+++ thingproject/models.py
@@ -9,10 +9,10 @@ class Thing(AbstractUser):
      description = models.CharField(unique= False,blank=True,max_length=120)
      quantity = models.IntegerField(
            validators=[MaxValueValidator(
-               limitValue=100
+               limit_value=100
            ),
                        MinValueValidator(
-                       limitValue=0
+                       limit_value=0
                        )
 
            ]
