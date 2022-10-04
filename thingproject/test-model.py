839e63ca753548144b69fe587b162c6656559948
Gonçalo  Azevedo <zalosantos02@gmail.com>
2022-10-04 09:05:50 +0100

    bruh

--- thingproject/test-model.py
+++ thingproject/test-model.py
@@ -3,7 +3,7 @@
 
 from django.core.exceptions import ValidationError
 from django.test import TestCase
-from things.models import Thing
+from .models import Thing
 
 class ModelTest(TestCase):
     def setUp(self):
